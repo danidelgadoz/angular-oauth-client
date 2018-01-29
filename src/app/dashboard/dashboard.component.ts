276a93d8568eece7ece711e318520d9fa88aab93
Daniel Delgado <dedd1993@gmail.com>
2018-01-29 15:08:10 -0500

    removing comments, http exchange code for token is working

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -59,53 +59,6 @@ export class DashboardComponent implements OnInit {
       'response_type': 'code',
       'code': code
     };
-
-    // const config = {
-    //   params: {
-    //     grant_type: 'password',
-    //     username: d.client_id,
-    //     password: d.client_secret
-
-    //   },
-    //   headers: {
-    //     Authorization: 'Basic ' + Base64.encode('secret-client' + ':' + 'secret')
-    //   }
-    // };
-
-    // $.post(`http://10.36.70.64:8087/oauth/token?grant_type=authorization_code&client_id=prime-front-end-key&client_secret=49b67f2c-c662-11e7-a3b6-0242ac120003&redirect_uri=http://10.36.71.183:4200/dashboard&code=${code}`,
-    // {},
-    // function(data, status){
-    //     console.log(data);
-    // });
-
-    // $.post(`http://10.36.70.64:8087/oauth/token`,
-    // {
-    //   'grant_type': 'authorization_code',
-    //   'client_id': 'prime-front-end-key',
-    //   'client_secret': '49b67f2c-c662-11e7-a3b6-0242ac120003',
-    //   'redirect_uri': 'http://10.36.71.183:4200/dashboard',
-    //   'code': code,
-    // },
-    // function(data, status){
-    //     console.log(data);
-    // });
-
-    // const settings = {
-    //   'async': true,
-    //   'crossDomain': true,
-    //   'url': 'http://10.36.70.64:8087/oauth/token',
-    //   'method': 'POST',
-    //   'headers': {
-    //     'content-type': 'application/json'
-    //   },
-    //   'processData': false,
-    //   'data': "{\"grant_type\":\"authorization_code\",\"client_id\": \"prime-front-end-key\",\"client_secret\": \"49b67f2c-c662-11e7-a3b6-0242ac120003\",\"code\": \"ubWVTX\",\"redirect_uri\": \"http://10.36.71.183:4200/dashboard\"}"
-    // };
-
-    // $.ajax(settings).done(function (response) {
-    //   console.log(response);
-    // });
   }
-
 
 }
--- src/app/services/oauth.service.ts
+++ src/app/services/oauth.service.ts
@@ -17,7 +17,7 @@ export class OauthService {
     this.windowLocation = window.location;
 
     this.data = {
-      'oauth_url': '10.36.70.64:8087',
+      'oauth_url': '192.168.43.149:8087',
       'client_id': 'prime-front-end-key',
       'scope': null,
       'redirect_uri': `${this.windowLocation.origin}/dashboard`,
@@ -40,10 +40,8 @@ export class OauthService {
     };
 
     const url = `http://${d.oauth_url}/oauth/token?grant_type=${d.grant_type}&client_id=${d.client_id}&redirect_uri=${d.redirect_uri}&code=${d.code}`;
-    // const url = `prime-front-end-key:49b67f2c-c662-11e7-a3b6-0242ac120003@10.36.70.64:8087/oauth/token?grant_type=authorization_code&client_id=prime-front-end-key&redirect_uri=http://10.36.71.183:4200/dashboard&code=fDlIK2
 
-    // return this.http.post(url, {}, {headers: this.headers})
-    return this.http.post(url, {})
+    return this.http.post(url, {}, {headers: this.headers})
     .map((response: Response) => {
         const body = response.json();
         console.log('OauthService response fron client service', body);
